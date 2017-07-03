ad849279d71e9b48258281e51bf325eb32a4d9ff
junichim <junichim@users.noreply.github.com>
2017-07-03 17:45:57 +0900

    文字列リテラルの『凡例』をリソースで定義しなおした

--- app/src/main/java/com/mori_soft/escape/dialog/LegendDialogFragment.java
+++ app/src/main/java/com/mori_soft/escape/dialog/LegendDialogFragment.java
@@ -87,7 +87,7 @@ public class LegendDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("凡例");
+        builder.setTitle(R.string.legend);
 
         View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_legend, null);
         ListView lv = (ListView) v.findViewById(R.id.legend_list);
