984f9bf745445c963ccbef0ad639790ec48b2b94
junichim <junichim@users.noreply.github.com>
2021-04-16 22:05:44 +0900

    Fragment の状態によっては getContext がnullになることへ対応

--- app/src/main/java/com/mori_soft/escape/Util/ConnectionUtil.java
+++ app/src/main/java/com/mori_soft/escape/Util/ConnectionUtil.java
@@ -9,10 +9,16 @@ public class ConnectionUtil {
     /**
      * WiFi でネットワークが接続されているか否か
      *
+     * context が null の場合は常に false として扱う
      * @param context
      * @return
      */
     public static boolean isWiFiConnected(Context context) {
+
+        // Fragment の状態によっては null となる場合があるため
+        if (context == null) {
+            return false;
+        }
 
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo info = cm.getActiveNetworkInfo();
